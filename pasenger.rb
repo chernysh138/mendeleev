d8f043cd347db0dffa96afc78c09264a0ebd9104
serhii <chernishsache@gmail.com>
2024-08-02 17:35:39 +0200

    v.1

---
+++ pasenger.rb
@@ -0,0 +1,9 @@
+pas = {
+  :ticket => "364897256",
+  :depart => "roma",
+  :destin => "napoli",
+  :passport => "EN138",
+  :name => "giorgio",
+  :cogname => "pupo",
+}
+puts "ваш билет номер #{pas[:ticket]} \n отправление из #{pas[:depart]}, \n прибытие в #{pas[:destin]} \n на имя:#{pas[:name]} фамилия:#{pas[:cogname]}  \n номер пасопрта #{pas[:passport]}"
